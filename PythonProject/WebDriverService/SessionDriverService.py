5c73825153f3ee7be9bf6df36b3106e86c54623c
yangfl <yangfu132@163.com>
2020-05-28 14:39:08 +0800

    demo:更新admin

--- PythonProject/WebDriverService/SessionDriverService.py
+++ PythonProject/WebDriverService/SessionDriverService.py
@@ -11,8 +11,6 @@ class SessionDriverService(object):
         self.session = Session();
         self.logger = get_logger(sharedFileBusiness.log_dir + 'SessionDriverService.log');
         
-    
-        
     def post(self,url,param):
         result = '';
         self.main_url = url;
@@ -25,3 +23,17 @@ class SessionDriverService(object):
         #else 
         
         return result;
+    
+    def loginAdmin(self,url,userID,loginKey):
+        proLoginEnv = url + 'system/login.login';
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0'
+        headers = {'User-Agent':user_agent};
+        r = self.session.post(proLoginEnv, {'loginId': userID, 'password': loginKey},headers);
+        print(r.status_code)
+        if r.status_code != 200:
+            self.logger.warn('error login failed, precess stop');
+            self.logged = False;
+        else:
+            self.logged = True;
+            
+        return self.logged;
